adb1ca8fe06aada19c891dcc8005e4231f6ff6dd
Álvaro Troncoso <alvaro.mc2@gmail.com>
2017-08-12 13:57:20 -0400

    add vue router

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.container
+  h1.title Nosotros
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      track: {}
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/TrackDetail.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  pm-track(:track)
+</template>
+
+<script>
+import trackService from '@/services/track'
+import PmTrack from '@/components/Track.vue'
+
+export default {
+  components: { PmTrack },
+  data () {
+    return {
+      track: {}
+    }
+  },
+  created () {
+    const id = this.$route.params.id
+
+    trackService.getById(id)
+      .then(res => {
+        console.log(res)
+      })
+  }
+}
+</script>
+
+<style lang="css">
+</style>
